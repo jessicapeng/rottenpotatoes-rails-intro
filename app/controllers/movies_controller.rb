166d456b480fac98068d5113e0b0e69e4bc580e9
jp3864 Jessica Peng <jp3864@columbia.edu>
2021-10-12 20:24:30 +0000

    reverted to original

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,4 @@
+
 class MoviesController < ApplicationController
 
   def show
@@ -7,38 +8,52 @@ class MoviesController < ApplicationController
   end
 
    def index
-     if params[:homepage] != '1' # came from another page
-       if session[:ratings] # check to make sure ratings set previously to create hash 
+    # if params == nil && session != nil
+    #   redirect_to movies_path(ratings: @all_ratings,)
+   #  end 
+
+     if params[:home] != '1' # came from another page
+       if session[:ratings]
          params[:ratings] = Hash[session[:ratings].collect{|item| [item, "1"]}]
        end 
-       redirect_to movies_path(sort:session[:sort], ratings: params[:ratings], homepage: '1') # redirect
+       redirect_to movies_path(sort:session[:sort], ratings: params[:ratings], home: '1')
      else
        
-       # ratings 
-       @all_ratings = Movie.all_ratings  
-       if params[:ratings] == nil # unchecked 
-         session[:ratings] = @ratings_to_show # but ratings to show here 
+     @all_ratings = Movie.all_ratings  
+     if params[:ratings] == nil && session[:ratings] == nil # first time visiting page 
+         @ratings_to_show = @all_ratings 
+         session[:ratings] = @ratings_to_show # put ratings to show here 
          @ratingsh = Hash[@all_ratings.collect{|item| [item, "1"]}]
-       else # replace current checked 
-          session[:ratings] = params[:ratings].keys 
-          @ratingsh =  Hash[session[:ratings].collect{|item| [item, "1"]}]
-       end 
+       
+     elsif params[:ratings] == nil && params[:home] == '1' # clicked from refresh button
+       @ratings_to_show = @all_ratings 
+       session[:ratings] = @ratings_to_show # but ratings to show here 
+       @ratingsh = Hash[@all_ratings.collect{|item| [item, "1"]}]
+       
+     elsif params[:ratings] == nil # clicked from movies page 
+       params[:ratings] = session[:ratings]
+       @ratings_to_show = session[:ratings]
+       
+     else # replace sessions 
+        session[:ratings] = params[:ratings].keys 
+        @ratingsh =  Hash[session[:ratings].collect{|item| [item, "1"]}]
+        @ratings_to_show = session[:ratings]
+     end 
      
-       # sessions 
-       if params[:sort] || session[:sort]
-          if params[:sort]
-            session[:sort] = params[:sort] 
-          end
-          if session[:sort] == "title"
-             @movie_colortile = "hilite bg-warning"
-          else
-             @date_colortile = "hilite bg-warning"
-          end
-        @movies = Movie.sorted_movies(session[:sort], session[:ratings])
-      else
-        @movies = Movie.with_ratings(session[:ratings])  
-      end
-      @ratings_to_show = session[:ratings] 
+     if params[:sort] || session[:sort]
+        if params[:sort]
+          session[:sort] = params[:sort] 
+        end
+        if session[:sort] == "title"
+           @movie_colortile = "hilite bg-warning"
+        else
+           @date_colortile = "hilite bg-warning"
+        end
+      @movies = Movie.sorted_movies(session[:sort], session[:ratings])
+    else
+      @movies = Movie.with_ratings(session[:ratings])  
+    end
+    @ratings_to_show = session[:ratings] || {} || param[:ratings].keys
     end 
   end   
 
